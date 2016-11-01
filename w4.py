b2f4201384dcaaa58f5893c0cee7d439c31ccf28
s40523127 <40523127@gm.nfu.edu.tw>
2016-11-01 21:41:49 +0800

    嗯?

--- w4.py
+++ w4.py
@@ -1,1 +1,10 @@
-print("s40523127")
+'''
+mystring = "scrum-1444"
+print(mystring)
+'''
+
+thetext = input("Enter some text ")
+print ("This is what you entered:", "test", sep="---")
+print (thetext)
+
+
